2a67e5931078556bf72425539b6523b331a2220e
fpellet <git@florentpellet.com>
2019-01-02 00:48:05 +0100

    Refactor program

--- YubikeyDetector/Program.fs
+++ YubikeyDetector/Program.fs
@@ -1,68 +1,138 @@
 module YubikeyDetector
 
 open System
-open System.Diagnostics
-open System.Management
-open Microsoft.Win32
-open System.Collections.Generic
 
-let yubikeys = HashSet<string>()
+type Disposables(disposables: IDisposable list) =
+    interface IDisposable with
+        member __.Dispose() = 
+            disposables |> List.iter (fun d -> d.Dispose())
 
-let debugSession _ =
-    use searcher = new ManagementObjectSearcher("select * from Win32_LogonSession")
-    searcher.Get() |> Seq.cast<ManagementObject> |> Seq.iter (fun d -> printfn "LoginId=%A LogonType=%A Status=%A" d.["LogonId"] d.["LogonType"] d.["Status"])
-    use searcher2 = new ManagementObjectSearcher("select * from Win32_LoggedOnUser")
-    searcher2.Get() |> Seq.cast<ManagementObject> |> Seq.iter (fun d -> printfn "Antecedent=%A Dependent=%A" d.["Antecedent"] d.["Dependent"])
+module WmiUsbDevices =
+    open System.Management
 
-let lockSession _ =
-    Process.Start("rundll32.exe", "user32.dll,LockWorkStation").WaitForExit()
-    printfn "Session locked"
-    debugSession ()
+    let private listenEvents query (listener: EventArrivedEventArgs -> unit) =
+        let watcher = new ManagementEventWatcher()
+        let subscription = watcher.EventArrived.Subscribe(listener)
+        watcher.Query <- new WqlEventQuery(query)
+        watcher.Start()
 
-let searchYubiKeys _ =
-    use searcher = new ManagementObjectSearcher("SELECT * FROM Win32_PnPEntity WHERE Caption LIKE '%yubico%'")
-    searcher.Get() |> Seq.cast<ManagementObject> |> Seq.map (fun d -> d.["DeviceID"] |> string) |> Seq.toList
+        new Disposables([watcher; subscription])
 
-let onDeviceConnected _ =
-    searchYubiKeys() |> Seq.iter (fun key -> if yubikeys.Add(key) then printfn "%s connected!" key)
+    let private search (query: string) parser =
+        use searcher = new ManagementObjectSearcher(query)
+        searcher.Get() |> Seq.cast<ManagementObject> |> Seq.map parser |> Seq.toList
 
-let onDeviceDisconnected _ =
-    yubikeys |> Seq.except (searchYubiKeys()) |> Seq.toList |> List.iter (fun key -> 
-        yubikeys.Remove(key) |> ignore
-        printfn "%s disconnected!" key)
+    type DeviceChanges =
+        | NewDeviceConnected
+        | DeviceDisconnected
 
-    if yubikeys.Count = 0
-    then lockSession()
+    let private DeviceChangesParser (evt: EventArrivedEventArgs) =
+        match evt.NewEvent.["EventType"] :?> uint16 with
+        | 2us -> Some NewDeviceConnected
+        | 3us -> Some DeviceDisconnected
+        | _ -> None
 
-let onEventRaised (evt: EventArrivedEventArgs) =
-    match evt.NewEvent.Properties |> Seq.cast<PropertyData> |> Seq.find (fun p -> p.Name = "EventType") |> fun p -> p.Value.ToString() with
-    | "2" -> onDeviceConnected()
-    | "3" -> onDeviceDisconnected()
-    | _ -> ()
+    let listenDeviceChanges (listener: DeviceChanges -> unit) = 
+        DeviceChangesParser >> Option.bind (listener >> Some) >> ignore
+        |> listenEvents "SELECT * FROM Win32_DeviceChangeEvent"
+        
+    type YubikeyId = string
 
-let onSessionSwitch _ (evt: SessionSwitchEventArgs) =
-    printfn "Session %A" evt.Reason
+    let searchYubikeys () : YubikeyId list =
+        search "SELECT * FROM Win32_PnPEntity WHERE Caption LIKE '%yubico%'" (fun device -> device.["DeviceID"] :?> string)
 
-    match evt.Reason with
-    | SessionSwitchReason.SessionLogon
-    | SessionSwitchReason.SessionUnlock -> 
-        if yubikeys.Count = 0
-        then lockSession()
-    | _ -> ()
+module Yubikeys =
+    open WmiUsbDevices
 
+    type Changes =
+        | NewYubikeyConnected of YubikeyId
+        | YubikeyDisconnected of YubikeyId
+
+    let private knownConnectedKeys = System.Collections.Generic.HashSet<YubikeyId>()
+
+    let private onKeyConnected listener key =
+        knownConnectedKeys.Add key |> ignore
+        NewYubikeyConnected key |> listener
+
+    let private onKeyDisconnected listener key =
+        knownConnectedKeys.Remove key |> ignore
+        YubikeyDisconnected key |> listener
+
+    let private onDeviceConnected listener =
+        searchYubikeys () 
+        |> Seq.filter (knownConnectedKeys.Contains >> not)
+        |> Seq.iter (onKeyConnected listener)
+
+    let private onDeviceDisconnected listener =
+        knownConnectedKeys
+        |> Seq.except (searchYubikeys ())
+        |> Seq.toList
+        |> Seq.iter (onKeyDisconnected listener)
+
+    let private onEventRaised listener = function
+        | NewDeviceConnected -> onDeviceConnected listener
+        | DeviceDisconnected -> onDeviceDisconnected listener
+
+    let private registerCurrentDevice = onDeviceConnected
+
+    let watch listener = 
+        let watcher = listenDeviceChanges (onEventRaised listener)
+        registerCurrentDevice listener
+        watcher
+
+    let hasConnectedKey () =
+        knownConnectedKeys.Count > 0
+
+module Sessions =
+    open System.Diagnostics
+    open Microsoft.Win32
+
+    let lock () =
+        use command = Process.Start("rundll32.exe", "user32.dll,LockWorkStation")
+        command.WaitForExit()
+        
+    let private onSessionSwitch onUnlock _ (evt: SessionSwitchEventArgs) =
+        printfn "Session %A" evt.Reason
+
+        match evt.Reason with
+        | SessionSwitchReason.SessionLogon
+        | SessionSwitchReason.SessionUnlock -> onUnlock ()
+        | _ -> ()
+
+    let watch onUnlock =
+        SystemEvents.SessionSwitch.AddHandler(SessionSwitchEventHandler(onSessionSwitch onUnlock));
+        
+
+module Watcher =
+    open Yubikeys
+
+    let private lockSessionIfNoKey () =
+        match hasConnectedKey () with
+        | true -> ()
+        | false ->
+            Sessions.lock ()
+            printfn "No key => Session locked"
+
+    let private onKeyChanged = function
+        | NewYubikeyConnected id ->
+            printfn "%s connected" id
+
+        | YubikeyDisconnected id ->
+            printfn "%s disconnected" id
+            lockSessionIfNoKey ()
+        
+    let run () =
+        let keysWatcher = Yubikeys.watch onKeyChanged
+        Sessions.watch lockSessionIfNoKey
+        lockSessionIfNoKey ()
+        keysWatcher
+
+let succeedExitCode = 0
+   
 [<EntryPoint>]
 let main _ =
-    use watcher = new ManagementEventWatcher()
-    let query = new WqlEventQuery("SELECT * FROM Win32_DeviceChangeEvent")
-    use _subcribe = watcher.EventArrived.Subscribe(onEventRaised)
-    watcher.Query <- query
-    watcher.Start()
-
-    onDeviceConnected ()
-    debugSession ()
-
-    SystemEvents.SessionSwitch.AddHandler(SessionSwitchEventHandler(onSessionSwitch));
+    use _watcher = Watcher.run ()
 
     Console.ReadLine() |> ignore
 
-    0
+    succeedExitCode
